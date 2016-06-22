a39da9217d7c88a7f7300b1f9d59234ddceef567
yuichi morisaki <ychmrsk@gmail.com>
2016-06-22 15:58:55 +0900

    add

---
+++ chap11/timeclnt.c
@@ -0,0 +1,50 @@
+/* timeclnt.c
+
+   usage: timeclnt <hostname> <port>
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define oops(msg) { perror(msg); exit(1); }
+
+int main(int ac, char *av[])
+{
+  struct sockaddr_in servadd;
+  struct hostent *hp;
+  int sock_id, sock_fd;
+  char message[BUFSIZ];
+  int messlen;  /* message length */
+  
+  /* step1. make socket */
+  sock_id = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock_id == -1)
+    oops("socket");
+
+  /* step2. connect to server */
+  bzero(&servadd, sizeof(servadd));
+  hp = gethostbyname(av[1]);
+  if (hp == NULL)
+    oops(av[1]);
+  bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr, hp->h_length);
+  servadd.sin_port = htons(atoi(av[2]));
+  servadd.sin_family = AF_INET;
+
+  if (connect(sock_id, (struct sockaddr *)&servadd, sizeof(servadd)) != 0)
+    oops("connect");
+
+  /* step3. recieve data from server, and close connection. */
+  messlen = read(sock_id, message, BUFSIZ);
+  if (messlen == -1)
+    oops("read");
+  if (write(1, message, messlen) != messlen)  /* stdout */
+    oops("write");
+  close(sock_id);
+}
