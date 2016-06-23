1127d1798e3debcc6fad1c5576c590d5be2c686d
yuichi morisaki <ychmrsk@gmail.com>
2016-06-24 01:13:15 +0900

    first commit

---
+++ chap14/hello_multi.c
@@ -0,0 +1,33 @@
+/* hello_multi.c
+   cc hello_multi.c -lpthread -o hello_multi
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NUM 5
+
+int main(void)
+{
+  pthread_t t1, t2;
+  
+  void *print_msg(void *);
+
+  pthread_create(&t1, NULL, print_msg, (void *)"hello");
+  pthread_create(&t2, NULL, print_msg, (void *)"world\n");
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+}
+
+void *print_msg(void *m)
+{
+  char *cp = (char *)m;
+  int i;
+  for (i = 0; i < NUM; i++) {
+    printf("%s", cp);
+    fflush(stdout);
+    sleep(1);
+  }
+  return NULL;
+}
